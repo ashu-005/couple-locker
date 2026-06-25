1c68a088c4f8b476905e5a021d5e8e73d716be7d
ASHMIT YADAV <ashmitbro7@gmail.com>
2026-06-25 12:48:56 +0530

    added via upload

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
+
+connectDB();
+
+app.listen(5000, () => {
+    console.log("Server Running");
+});
