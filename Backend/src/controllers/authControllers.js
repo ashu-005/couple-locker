f34a1f2dc527c178c4ad627e64a7dfb6675e4111
ASHMIT YADAV <ashmitbro7@gmail.com>
2026-06-25 12:47:20 +0530

    Implement user login controller

---
+++ Backend/src/controllers/authControllers.js
@@ -0,0 +1,21 @@
+const User = require("../models/User");
+
+const login = async (req, res) => {
+
+    const { mobile } = req.body;
+
+    const user = await User.findOne({ mobile });
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    return res.status(200).json({
+        message: "Login Successful",
+        user
+    });
+}
+
+module.exports = { login };
